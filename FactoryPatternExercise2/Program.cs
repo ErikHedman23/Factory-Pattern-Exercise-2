a4a5f44bd86c10607de878fb1d11c8a1348a59ed
Erik Hedman <erikdavid.hedman21@outlook.com>
2023-08-07 13:00:31 -0700

    Created a while loop

--- FactoryPatternExercise2/Program.cs
+++ FactoryPatternExercise2/Program.cs
@@ -4,12 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("What database would you like to use: a Mongo, SQL, or List?");
-            var userInput = Console.ReadLine();
-            var dataBase = DataAccessFactory.GetDataAccessType(userInput);
-            dataBase.LoadData();
-            dataBase.SaveData();
-            
+            int count = 0;
+            while (count <= 3)
+            {
+
+
+                Console.WriteLine("What database would you like to use: a Mongo, SQL, or List?");
+                var userInput = Console.ReadLine();
+                var dataBase = DataAccessFactory.GetDataAccessType(userInput);
+                dataBase.LoadData();
+                dataBase.SaveData();
+                count++;
+            }
         }
     }
 }
